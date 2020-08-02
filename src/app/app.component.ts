698f688971e9e6a741cd3bf20373d766bd866106
BriaanSz <briaansorzu@gmail.com>
2020-08-01 19:20:52 -0500

    Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+const urlImg = 'assets/logos/{name}.png'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sorzu-links';
+  public socialLinks: Array<{ name: string, url: string, img: string }> = [
+    { name: 'Facebook', url: 'https://fb.com/sorzudev', img: urlImg.replace('{name}', 'fb') },
+    { name: 'Twitter', url: 'https://twitter.com/sorzudev', img: urlImg.replace('{name}', 'tw') },
+    { name: 'YouTube', url: 'https://www.youtube.com/channel/UCYqHYu0S6knon0wSOZgTeSw', img: urlImg.replace('{name}', 'yt') },
+    { name: 'Instagram', url: 'https://instagram.com/sorzudev', img: urlImg.replace('{name}', 'insta') },
+  ];
+
+  public openUrl(url: string) {
+    window.open(url);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
